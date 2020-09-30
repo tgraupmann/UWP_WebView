7440b71f692f1319532589a827b4a35c865d77ae
Timothy Graupmann <tim.graupmann@razer.com>
2020-09-30 10:47:09 -0700

    Added sample

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Core.Preview;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP_WebView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public class PageArgs
+        {
+            public string StartUrl = string.Empty;
+            public string EndUrl = string.Empty;
+            public delegate Task ContentCallback(string content);
+            public ContentCallback OnComplete = null;
+            public ContentCallback OnError = null;
+        }
+
+        private PageArgs _mPageArgs = null;
+
+
+        public LoginPage()
+        {
+            this.InitializeComponent();
+            SystemNavigationManagerPreview.GetForCurrentView().CloseRequested += (s, e) =>
+            {
+                // Code run when the window is closing
+                if (_mPageArgs != null)
+                {
+                    if (_mPageArgs.OnError != null)
+                    {
+                        _mPageArgs.OnError("User closed the window!");
+                    }
+                }
+            };
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null && e.Parameter is PageArgs)
+            {
+                _mPageArgs = (PageArgs)e.Parameter;
+                _mWebView.Language = "en-US";
+                Windows.UI.Xaml.Media.TranslateTransform transform = new Windows.UI.Xaml.Media.TranslateTransform();
+                transform.X = -200;
+                _mWebView.RenderTransform = transform;
+                _mWebView.Source = new Uri(_mPageArgs.StartUrl);
+            }
+
+        }
+
+        private async void WebView_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            if (_mPageArgs != null)
+            {
+                string url = _mWebView.Source.ToString();
+                if (url.StartsWith("https://www.twitch.tv/"))
+                {
+                    if (url.StartsWith("https://www.twitch.tv/login"))
+                    {
+                    }
+                    else
+                    {
+                        //invoke callback and close window
+                        if (_mPageArgs.OnComplete != null)
+                        {
+                            string html = await _mWebView.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
+                            await _mPageArgs.OnComplete(html);
+                        }
+                        _mPageArgs = null;
+                        Window.Current.Close();
+                    }
+                }
+                else
+                {
+                    if (url.StartsWith(_mPageArgs.EndUrl))
+                    {
+                        if (_mPageArgs.OnComplete != null)
+                        {
+                            string html = await _mWebView.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
+                            await _mPageArgs.OnComplete(html);
+                            _mPageArgs = null;
+                            Window.Current.Close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +16,82 @@ namespace UWP_WebView
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool _mPendingResult = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(600, 400);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private async Task OnComplete(string content)
+        {
+            if (_mPendingResult)
+            {
+                _mPendingResult = false;
+
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    if (!string.IsNullOrEmpty(content))
+                    {
+                        const int maxLength = 1024;
+                        if (content.Length > maxLength)
+                        {
+                            _mTxtResult.Text = content.Substring(0, maxLength);
+                        }
+                        else
+                        {
+                            _mTxtResult.Text = content;
+                        }
+                    }
+                    else
+                    {
+                        _mTxtResult.Text = "(Empty)";
+                    }
+                });
+            }
+        }
+
+        private async Task OnError(string error)
+        {
+            _mPendingResult = false;
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                _mTxtResult.Text = error;
+            });
+        }
+
+        private async void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            _mPendingResult = true;
+
+            LoginPage.PageArgs pageArgs = new LoginPage.PageArgs();
+            pageArgs.StartUrl = _mTxtUrl.Text;
+            pageArgs.EndUrl = "https://www.twitch.com/";
+            pageArgs.OnComplete = OnComplete;
+            pageArgs.OnError = OnError;
+
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(LoginPage), pageArgs);
+                Window.Current.Content = frame;
+                // You have to activate the window in order to show it later.
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mTxtUrl.Text = string.Format("https://example.com/OAuth2?guid={0}", Guid.NewGuid());
         }
     }
 }
